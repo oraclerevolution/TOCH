fae8a2f1baab528f33eb85bb5ef933f09ccb6f96
Ecole NaN <nan@iMac-13.local>
2018-10-12 17:51:56 +0000

    ma bdd

--- mairie.sql
+++ mairie.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Ven 12 Octobre 2018 à 14:51
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+-- Hôte : localhost
+-- Généré le :  ven. 12 oct. 2018 à 19:51
+-- Version du serveur :  10.1.28-MariaDB
+-- Version de PHP :  5.6.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `mairie`
@@ -26,11 +28,10 @@ SET time_zone = "+00:00";
 -- Structure de la table `communes`
 --
 
-CREATE TABLE IF NOT EXISTS `communes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom_commune` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `communes` (
+  `id` int(11) NOT NULL,
+  `nom_commune` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -38,13 +39,12 @@ CREATE TABLE IF NOT EXISTS `communes` (
 -- Structure de la table `mail_envoyes`
 --
 
-CREATE TABLE IF NOT EXISTS `mail_envoyes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `mail_envoyes` (
+  `id` int(11) NOT NULL,
   `register_date` datetime NOT NULL,
   `sujet` varchar(50) NOT NULL,
-  `le_mail` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `le_mail` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -52,12 +52,11 @@ CREATE TABLE IF NOT EXISTS `mail_envoyes` (
 -- Structure de la table `mairie_tables`
 --
 
-CREATE TABLE IF NOT EXISTS `mairie_tables` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `mairie_tables` (
+  `id` int(11) NOT NULL,
   `nom_mairie` varchar(50) NOT NULL,
-  `date_register` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `date_register` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -65,15 +64,14 @@ CREATE TABLE IF NOT EXISTS `mairie_tables` (
 -- Structure de la table `newsletters`
 --
 
-CREATE TABLE IF NOT EXISTS `newsletters` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `newsletters` (
+  `id` int(11) NOT NULL,
   `mail` varchar(50) NOT NULL,
-  `register_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `register_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `newsletters`
+-- Déchargement des données de la table `newsletters`
 --
 
 INSERT INTO `newsletters` (`id`, `mail`, `register_date`) VALUES
@@ -86,10 +84,9 @@ INSERT INTO `newsletters` (`id`, `mail`, `register_date`) VALUES
 -- Structure de la table `services_acceptes`
 --
 
-CREATE TABLE IF NOT EXISTS `services_acceptes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `services_acceptes` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -97,10 +94,9 @@ CREATE TABLE IF NOT EXISTS `services_acceptes` (
 -- Structure de la table `services_attentes`
 --
 
-CREATE TABLE IF NOT EXISTS `services_attentes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `services_attentes` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -108,16 +104,15 @@ CREATE TABLE IF NOT EXISTS `services_attentes` (
 -- Structure de la table `services_effectues`
 --
 
-CREATE TABLE IF NOT EXISTS `services_effectues` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `services_effectues` (
+  `id` int(11) NOT NULL,
   `nom_demandeur` varchar(100) NOT NULL,
   `numero_cni` varchar(100) NOT NULL,
   `email_demandeur` varchar(30) NOT NULL,
   `tel_demandeur` varchar(20) NOT NULL,
   `nom_services` varchar(50) NOT NULL,
-  `register_date` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `register_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -125,10 +120,9 @@ CREATE TABLE IF NOT EXISTS `services_effectues` (
 -- Structure de la table `services_refuses`
 --
 
-CREATE TABLE IF NOT EXISTS `services_refuses` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `services_refuses` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -136,24 +130,21 @@ CREATE TABLE IF NOT EXISTS `services_refuses` (
 -- Structure de la table `services_tables`
 --
 
-CREATE TABLE IF NOT EXISTS `services_tables` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `services_tables` (
+  `id` int(11) NOT NULL,
   `titre` varchar(30) NOT NULL,
   `descr` text NOT NULL,
   `photo` varchar(100) NOT NULL,
-  `register_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+  `register_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `services_tables`
+-- Déchargement des données de la table `services_tables`
 --
 
 INSERT INTO `services_tables` (`id`, `titre`, `descr`, `photo`, `register_date`) VALUES
-(8, 'Declaration de mariage', 'rÃ©cuperer vos extrait de naissance ici', 'image1.jpg', '2018-10-05 16:13:26'),
-(9, 'DÃ©claration de naissance', 'DÃ©clarez dÃ©sormais vos enfants ici', 'image2.jpg', '2018-10-05 16:14:09'),
-(10, 'DÃ©claration de dÃ©cÃ¨s', 'DÃ©clarez vos dÃ©cÃ¨s ici maintenant', 'image3.jpg', '2018-10-05 16:14:49'),
-(11, 'Copie d''extrait de naissance', 'Faites la demande d''extrait de naissance ici dÃ¨s maintenant', 'image4.jpg', '2018-10-05 16:16:35');
+(9, 'DÃ©claration de mariage', 'DÃ©clarez vos mariage ici maintenant', 'abeille1.png', '2018-10-05 16:14:09'),
+(10, 'DÃ©claration de naissance', 'DÃ©clarez vos naissance ici maintenant', 'lab_separator.png', '2018-10-05 16:14:49');
 
 -- --------------------------------------------------------
 
@@ -161,24 +152,152 @@ INSERT INTO `services_tables` (`id`, `titre`, `descr`, `photo`, `register_date`)
 -- Structure de la table `users_tables`
 --
 
-CREATE TABLE IF NOT EXISTS `users_tables` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users_tables` (
+  `id` int(11) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `email` varchar(30) NOT NULL,
   `mot_de_passe` varchar(255) NOT NULL,
   `photo` varchar(30) NOT NULL,
   `ville` varchar(50) NOT NULL,
-  `register_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `register_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `users_tables`
+-- Déchargement des données de la table `users_tables`
 --
 
 INSERT INTO `users_tables` (`id`, `nom`, `email`, `mot_de_passe`, `photo`, `ville`, `register_date`) VALUES
 (4, 'oracle', 'assia.ngoran@uvci.edu.ci', 'test', 'profil.jpg', 'anyama', '2018-10-05 17:02:52'),
 (5, 'assia', 'assia.ngoran@uvci.edu.ci', 'bonjour', '2.jpg', 'anyama', '2018-10-08 18:10:51');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `communes`
+--
+ALTER TABLE `communes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `mail_envoyes`
+--
+ALTER TABLE `mail_envoyes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `mairie_tables`
+--
+ALTER TABLE `mairie_tables`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `newsletters`
+--
+ALTER TABLE `newsletters`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `services_acceptes`
+--
+ALTER TABLE `services_acceptes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `services_attentes`
+--
+ALTER TABLE `services_attentes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `services_effectues`
+--
+ALTER TABLE `services_effectues`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `services_refuses`
+--
+ALTER TABLE `services_refuses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `services_tables`
+--
+ALTER TABLE `services_tables`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users_tables`
+--
+ALTER TABLE `users_tables`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `communes`
+--
+ALTER TABLE `communes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `mail_envoyes`
+--
+ALTER TABLE `mail_envoyes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `mairie_tables`
+--
+ALTER TABLE `mairie_tables`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `newsletters`
+--
+ALTER TABLE `newsletters`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `services_acceptes`
+--
+ALTER TABLE `services_acceptes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `services_attentes`
+--
+ALTER TABLE `services_attentes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `services_effectues`
+--
+ALTER TABLE `services_effectues`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `services_refuses`
+--
+ALTER TABLE `services_refuses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `services_tables`
+--
+ALTER TABLE `services_tables`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT pour la table `users_tables`
+--
+ALTER TABLE `users_tables`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
